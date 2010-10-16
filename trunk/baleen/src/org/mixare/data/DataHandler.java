d143eb56d3764abedd28849ead5078cec4b3039f
killercentury <killercentury@74c3afd7-f914-e46b-abbe-8d2c5e82e858>
2010-10-16 00:01:02 +0000

    Initialization

---
+++ trunk/baleen/src/org/mixare/data/DataHandler.java
@@ -0,0 +1,54 @@
+package org.mixare.data;
+
+import java.net.URLDecoder;
+import java.util.ArrayList;
+
+import org.mixare.Marker;
+import org.mixare.MixView;
+
+import android.util.Log;
+
+/**
+ * DataHandler is the model which provides the Marker Objects.
+ * 
+ * DataHandler is also the Factory for new Marker objects.
+ */
+public class DataHandler {
+
+	private static final int MAX_OBJECTS = 50;
+	private ArrayList<Marker> markerList = new ArrayList<Marker>();
+
+	public void createMarker(String title, double latitude, double longitude, double elevation, String link) {
+		Log.d(MixView.TAG, "Debug: DataHandler - createMarker");
+		if (markerList.size() < MAX_OBJECTS) {
+			String URL = null;
+			if (link != null && link.length() > 0)
+				URL = "webpage:" + URLDecoder.decode(link);
+			Marker ma = new Marker(title, latitude, longitude, elevation, URL);
+			markerList.add(ma);
+			Log.d(MixView.TAG, "Debug: DataHandler - markerList.add");
+		}
+	}
+	
+	/**
+	 * @deprecated Nobody should get direct access to the list
+	 */
+	public ArrayList getMarkerList() {
+		return markerList;
+	}
+	
+	/**
+	 * @deprecated Nobody should get direct access to the list
+	 */
+	public void setMarkerList(ArrayList markerList) {
+		this.markerList = markerList;
+	}
+
+	public int getMarkerCount() {
+		return markerList.size();
+	}
+	
+	public Marker getMarker(int index) {
+		return markerList.get(index);
+	}
+}
